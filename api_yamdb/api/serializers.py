a2072be4876843d671ba6ef16becaa7d0be71ea1
Andrew Sokolov <ya.17bit@ya.ru>
2025-08-19 20:56:44 +0300

    Add CommentSerializer

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from reviews.models import Category, Genre, Title, Review
+from reviews.models import Category, Genre, Title, Review, Comment
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -26,3 +26,9 @@ class ReviewSerializer(serializers.ModelSerializer):
         model = Review
         fields = ('id', 'review_author', 'review_title', 'review_pub_date',
                   'review_text')
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ('id', 'comment_author', 'comment_pub_date', 'comment_text')
