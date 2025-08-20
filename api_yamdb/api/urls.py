825c8f4685c31224fd49fe71ccf193f149978994
Andrew Sokolov <ya.17bit@ya.ru>
2025-08-20 20:25:31 +0300

    Add urls Comment adm Rewiew

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 
 
 from .views import (
-    CategoryViewSet, GenreViewSet, TitleViewSet
+    CategoryViewSet, GenreViewSet, TitleViewSet, ReviewViewSet, CommentViewSet
 )
 
 
@@ -11,6 +11,17 @@ router_v1 = DefaultRouter()
 router_v1.register(r'categories', CategoryViewSet, basename='categories')
 router_v1.register(r'genres', GenreViewSet, basename='genres')
 router_v1.register(r'titles', TitleViewSet, basename='titles')
+
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/reviews',
+    ReviewViewSet,
+    basename='reviews'
+)
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<reviews_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
+)
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
