dfa2628a9a333d8b791b1800dcd90760543a047b
Aleksandra Peskova <peskoval88@gmail.com>
2025-08-20 15:52:51 +0300

    project-base pagination

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -57,6 +57,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 
+# Django Rest Framework settings
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5,
+}
 
 # Database
 
