9b6f289746c0a7cf924bcd1b1e376d0e75c253f8
Aleksandra Peskova <peskoval88@gmail.com>
2025-08-20 16:41:38 +0300

    searching for genres & categories

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,9 +1,12 @@
-from rest_framework import mixins, viewsets
+from rest_framework import filters, mixins, viewsets
 from django_filters.rest_framework import DjangoFilterBackend
 
 from .filters import TitleFilter
 from .serializers import (
-    CategorySerializer, GenreSerializer, TitlePostMethodSerializer, TitleSerializer
+    CategorySerializer,
+    GenreSerializer,
+    TitlePostMethodSerializer,
+    TitleSerializer
 )
 from reviews.models import Category, Genre, Title
 
@@ -20,18 +23,22 @@ class CreateDestroyListViewSet(
 class CategoryViewSet(CreateDestroyListViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('slug',)
 
 
 class GenreViewSet(CreateDestroyListViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('slug',)
 
 
 class TitleViewSet(viewsets.ModelViewSet):
     queryset = Title.objects.all()
     filter_backends = (DjangoFilterBackend,)
     filterset_class = TitleFilter
-    filterset_fields = ('name', 'year', 'category', 'genre')
+    filterset_fields = ('name', 'year',)
 
     def get_serializer_class(self):
         if self.request.method == 'GET':
