00e99a97db78d1cc9ca7f7c00e68b8073bc6c7b3
Aleksandra Peskova <peskoval88@gmail.com>
2025-08-21 15:39:18 +0300

    title fixed

--- api_yamdb/api/filters.py
+++ api_yamdb/api/filters.py
@@ -6,6 +6,8 @@ from reviews.models import Title
 class TitleFilter(django_filters.FilterSet):
     category = django_filters.CharFilter(field_name='category__slug')
     genre = django_filters.CharFilter(field_name='genre__slug')
+    name = django_filters.CharFilter(field_name='name')
+    year = django_filters.CharFilter(field_name='year')
 
     class Meta:
         model = Title
