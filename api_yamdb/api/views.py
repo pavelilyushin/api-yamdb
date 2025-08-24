7fe97b0d2048d587d9fafa696bdb006c574f7e8c
Pavel Ilyushin <me@pavelilyushin.ru>
2025-08-24 07:49:38 +0400

    добавлена пагинация

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -70,7 +70,7 @@ class ReviewViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         title = self.get_title()
-        return title.reviews.all()
+        return title.reviews.all().order_by('id')
 
     def perform_create(self, serializer):
         title = self.get_title()
@@ -97,7 +97,7 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         review = self.get_review()
-        return review.comments.all()
+        return review.comments.all().order_by('id')
 
     def perform_create(self, serializer):
         review = self.get_review()
