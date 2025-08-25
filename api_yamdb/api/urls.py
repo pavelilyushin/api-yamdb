06c4943bb895c8d964240ecec47a18c269d0d232
Pavel Ilyushin <me@pavelilyushin.ru>
2025-08-25 13:06:07 +0400

    fix: забыл v1

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -10,28 +10,26 @@ from .views import (
     CommentViewSet
 )
 
-router = DefaultRouter()
-router.register('users', UserViewSet)
-router.register(r'categories', CategoryViewSet, basename='categories')
-router.register(r'genres', GenreViewSet, basename='genres')
-router.register(r'titles', TitleViewSet, basename='titles')
-router.register(
+router_v1 = DefaultRouter()
+router_v1.register('users', UserViewSet)
+router_v1.register(r'categories', CategoryViewSet, basename='categories')
+router_v1.register(r'genres', GenreViewSet, basename='genres')
+router_v1.register(r'titles', TitleViewSet, basename='titles')
+router_v1.register(
     r'titles/(?P<title_id>\d+)/reviews',
     ReviewViewSet,
     basename='reviews'
 )
-router.register(
+router_v1.register(
     r'titles/(?P<title_id>\d+)/reviews/(?P<reviews_id>\d+)/comments',
     CommentViewSet,
     basename='comments'
 )
 
 urlpatterns = [
-    path('', include(router.urls)),
-    # path('auth/signup/', SignUpView.as_view(), name='signup'),
-    # path('auth/token/', TokenView.as_view(), name='token'),
+    path('', include(router_v1.urls)),
     path('v1/', include([
-        path('', include(router.urls)),
+        path('', include(router_v1.urls)),
         path('auth/signup/', SignUpView.as_view(), name='signup_v1'),
         path('auth/token/', TokenView.as_view(), name='token_v1'),
     ])),
