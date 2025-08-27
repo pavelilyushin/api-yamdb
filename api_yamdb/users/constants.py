f3db877e21b4c97b3df7669e6ab3f2526608e668
Pavel Ilyushin <me@pavelilyushin.ru>
2025-08-27 10:14:14 +0400

    add comments to user constants

--- api_yamdb/users/constants.py
+++ api_yamdb/users/constants.py
@@ -1,11 +1,15 @@
+# Константы для пользователей
 USER_EMAIL_MAX_LENGTH = 254
 USER_BIO_MAX_LENGTH = 500
 USER_ROLE_MAX_LENGTH = 20
 USER_CONFIRMATION_CODE_MAX_LENGTH = 100
 
+# Длина кода подтверждения
 CONFIRMATION_CODE_LENGTH = 6
 
+# Регулярное выражение для валидации username
 USERNAME_PATTERN = r'^[\w.@+-]+\Z'
 
 # Дополнительные константы для сериализаторов
+# Максимальная длина username
 USERNAME_MAX_LENGTH = 150
